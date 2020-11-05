fb910fbce2d9dd3dd73ad6eb3b4f7f119217261c
Urand1503 <59353483+Urand1503@users.noreply.github.com>
2020-11-05 00:06:39 -0600

    All done

--- src/singleThreadedClientTest/SingleThreadedClientTest.java
+++ src/singleThreadedClientTest/SingleThreadedClientTest.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package singleThreadedClientTest;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  *
@@ -11,4 +15,62 @@ package singleThreadedClientTest;
  */
 public class SingleThreadedClientTest {
     
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        InetAddress ClientAddress;
+        clientUserInterface.ClientUserInterface UserInterface= new clientUserInterface.ClientUserInterface();
+        singlethreadedclient.SingleThreadedClient client = new singlethreadedclient.SingleThreadedClient();
+        
+        UserInterface.display("d:\tDisconnect\n"
+                     + "t:\tTime\n"
+                     + "c:\tConnect\n");
+         String ToPrint = "";
+        while(true){
+            
+            String input = UserInterface.GetString();
+            
+            switch(input){
+                case "c":
+                    client.StartServer("localhost", 5555);
+                    break;
+                case "d":
+                    //client.StopServer();
+                    // need to send d to the server
+                    client.sendMsgToServer((byte) 'd');
+                    UserInterface.display("The client sucessfully disconnected");
+                    break;
+                case "t":
+                    //client.sendMsgToServer((byte) 't');
+                    if(client.sendMsgToServer((byte) 't')){
+                    UserInterface.display("Waiting to recieve time");}
+                    else{
+                    UserInterface.display("Msg not sent successfully");
+                    }
+                    //char [] arrayTime = new char[7];
+                    var arrayList = new ArrayList<String>();
+
+                    boolean check = true;
+                    while(check){
+                    
+                        byte recievedValue = client.GetMsgFromServer();
+                            var value = (char)recievedValue;
+                            String charValue = String.valueOf(value);
+                                arrayList.add(charValue);
+                        if(arrayList.size() == 7)
+                        {check = false;}
+                    }
+                    if(arrayList.size() == 7){
+                       
+                   for(int i =0; i< arrayList.size(); i++){
+                      ToPrint +=  arrayList.get(i);
+                   }
+                    }
+                  }
+             UserInterface.display(ToPrint);
+        }
+       
+        
+        
+    }
 }
--- src/singlethreadedclient/SingleThreadedClient.java
+++ src/singlethreadedclient/SingleThreadedClient.java
@@ -4,18 +4,93 @@
  * and open the template in the editor.
  */
 package singlethreadedclient;
-
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 /**
  *
  * @author Ngozi Francis Uranta
  */
 public class SingleThreadedClient {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    Socket socket;
+    InputStream in;
+    OutputStream out;
+    int port;
+   public SingleThreadedClient(){
+       in = null;
+       out = null;
+   }
+   
+   
+   public void StartServer(String host, int PortNum){
+   
+       try{
+           InetAddress ad =  InetAddress.getByName(host);
+           socket = new Socket(ad,PortNum);
+           if(socket !=null){System.out.println("Connected");}
+           in = socket.getInputStream();
+           out = socket.getOutputStream();
+       }
+       
+       catch(IOException e){
+           System.err.println("Cannot Connect to Server");
+       }
+   }
+   
+   public void StopServer(){
+       try{
+           socket.close();
+           this.in = null;
+           this.out = null;
+       }
+       catch(IOException e){
+           System.err.println("Cannot close stream or client socket; exiting");
+           System.exit(1);
+       }
+   
+   }
+   
+   public int getPort(){
+       
+       return socket.getLocalPort();
+   }
     
+   public void setPort(int portNum){
+   
+       port = portNum;
+   
+   }
+   
+   
+   public boolean sendMsgToServer(byte MsgToSend){
+       boolean returnValue = false;
+ 
+           try{
+           out.write(MsgToSend);
+           returnValue = true;
+           }
+           catch(IOException e){
+           System.err.println("Invalid Command");
+           returnValue = false;
+           }
+       return returnValue;
+   }
+   
+   
+   public byte GetMsgFromServer(){
+   
+        byte msg = (byte)-1;
+        
+        try{
+            msg = (byte)in.read();
+        }
+        catch(IOException i){
+            System.err.println("Inputstream empty");
+        }
+        finally{
+        return msg;
+        }
+   }
 }
